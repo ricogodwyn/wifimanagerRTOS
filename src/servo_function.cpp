86856fd8e29a9be9dce60841b01ed00094c0172f
ric <frederico.godwyn@gmail.com>
2024-12-31 14:40:12 +0700

    add servo to 6 channel

--- src/servo_function.cpp
+++ src/servo_function.cpp
@@ -4,11 +4,6 @@
 #define SERVOMIN 150 // This is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX 600 // This is the 'maximum' pulse length count (out of 4096)
 
-#define SERVO1_CHANNEL 0
-#define SERVO2_CHANNEL 1
-#define SERVO3_CHANNEL 2
-#define SERVO4_CHANNEL 3
-
 void moveServos(int position)
 {
     for (int channel = 0; channel < 4; channel++)
@@ -21,7 +16,7 @@ void moveServos(int position)
 
 void stopServos()
 {
-    for (int channel = 0; channel < 4; channel++)
+    for (int channel = 0; channel < 6; channel++)
     {
         pwm.setPWM(channel, 0, (SERVOMIN + SERVOMAX) / 2); // Set to neutral position
     }
